5dd2bf54da75c305a41549ebe5413a5fb268c1c6
Sunil Bansiwal <sunilssb786@gmail.com>
2020-05-21 14:47:20 +0530

    Update Jira.js

--- common/net/Jira.js
+++ common/net/Jira.js
@@ -40,16 +40,16 @@ class Jira {
   }
   
   async getIssueStatus (issueId) {
-    const { fields = [], expand = [] } = query
+   // const { fields = [], expand = [] } = query
 
     try {
       const res = await this.fetch('getIssueStatus', {
         pathname: `/rest/api/2/issue/${issueId}?fields=status`,
-        query: {
-          fields: fields.join(','),
-          expand: expand.join(','),
-        },
-      })
+//         query: {
+//           fields: fields.join(','),
+//           expand: expand.join(','),
+//         },
+       })
 
       return res
     } catch (error) {
