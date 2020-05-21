84d3ea5aa57e33ca97952e4d3a1774760aa9a3f5
sunil-bansiwal <sunil.bansiwal@razorpay.com>
2020-05-21 10:13:30 +0530

    Added packages

--- README.md
+++ README.md
@@ -1,1 +1,40 @@
-# find-jira-issue
+# Jira Find Issue Key
+Extract issue key from string
+
+For examples on how to use this, check out the [gajira-demo](https://github.com/atlassian/gajira-demo) repository
+> ##### Only supports Jira Cloud. Does not support Jira Server (hosted)
+
+## Usage
+
+> ##### Note: this action requires [Jira Login Action](https://github.com/marketplace/actions/jira-login)
+
+To find an issue key inside commit messages:
+```yaml
+- name: Find in commit messages
+  uses: atlassian/gajira-find-issue-key@master
+  with:
+    from: commits
+```
+
+----
+## Action Spec:
+
+### Environment variables
+- None
+
+### Inputs
+- `description` - Provide jsonpath for the GitHub event to extract issue from
+- `string` - Provide a string to extract issue key from
+- `from` - Find from predefined place (should be either 'branch', or 'commits', default is 'commits')
+
+### Outputs
+- `issue` - Key of the found issue
+
+### Reads fields from config file at $HOME/jira/config.yml
+- None
+
+### Writes fields to config file at $HOME/jira/config.yml
+- `issue` - a key of a found issue
+
+### Writes fields to CLI config file at $HOME/.jira.d/config.yml
+- `issue` - a key of a found issue
---
+++ index.js
@@ -0,0 +1,52 @@
+const fs = require('fs')
+const YAML = require('yaml')
+const core = require('@actions/core')
+
+const cliConfigPath = `${process.env.HOME}/.jira.d/config.yml`
+const configPath = `${process.env.HOME}/jira/config.yml`
+const Action = require('./action')
+
+// eslint-disable-next-line import/no-dynamic-require
+const githubEvent = require(process.env.GITHUB_EVENT_PATH)
+const config = YAML.parse(fs.readFileSync(configPath, 'utf8'))
+
+async function exec () {
+  try {
+    const result = await new Action({
+      githubEvent,
+      argv: parseArgs(),
+      config,
+    }).execute()
+
+    if (result) {
+      console.log(`Detected issueKey: ${result.issue}`)
+      console.log(`Saving ${result.issue} to ${cliConfigPath}`)
+      console.log(`Saving ${result.issue} to ${configPath}`)
+
+      // Expose created issue's key as an output
+      core.setOutput('issue', result.issue)
+
+      const yamledResult = YAML.stringify(result)
+      const extendedConfig = Object.assign({}, config, result)
+
+      fs.writeFileSync(configPath, YAML.stringify(extendedConfig))
+
+      return fs.appendFileSync(cliConfigPath, yamledResult)
+    }
+
+    console.log('No issueKeys found.')
+    core.setNeutral()
+  } catch (error) {
+    core.setFailed(error.toString())
+  }
+}
+
+function parseArgs () {
+  return {
+    event: core.getInput('event') || config.event,
+    string: core.getInput('string') || config.string,
+    from: core.getInput('from'),
+  }
+}
+
+exec()
