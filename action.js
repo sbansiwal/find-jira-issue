e411166caaedb24ca6dcba7289a00eb5cfc8e50b
Sunil Bansiwal <sunilssb786@gmail.com>
2020-05-21 10:46:42 +0530

    Update action.js

--- action.js
+++ action.js
@@ -37,7 +37,7 @@ module.exports = class {
       const issueStatus = await this.Jira.getIssueStatus(issueKey)
       
       if (issue) {
-        return { issue: issueStatus}
+        return { issue: issue}
       }
     }
   }
